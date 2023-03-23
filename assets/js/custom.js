5fc9463bc7f9799f5d990b225f6006fde9cb705f
keysparks <saravanan@keysparks.com>
2023-03-23 10:52:30 +0530

    commit 23032023

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -16,28 +16,28 @@
   });
 
 
-	$('.owl-banner').owlCarousel({
-		items:1,
-		loop:true,
-		dots: true,
-		nav: false,
-		autoplay: true,
-		margin:0,
-		  responsive:{
-			  0:{
-				  items:1
-			  },
-			  600:{
-				  items:1
-			  },
-			  1000:{
-				  items:1
-			  },
-			  1600:{
-				  items:1
-			  }
-		  }
-	})
+	// $('.owl-banner').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	dots: true,
+	// 	nav: false,
+	// 	autoplay: true,
+	// 	margin:0,
+	// 	  responsive:{
+	// 		  0:{
+	// 			  items:1
+	// 		  },
+	// 		  600:{
+	// 			  items:1
+	// 		  },
+	// 		  1000:{
+	// 			  items:1
+	// 		  },
+	// 		  1600:{
+	// 			  items:1
+	// 		  }
+	// 	  }
+	// })
 
     $('.owl-services').owlCarousel({
         items:4,
@@ -62,28 +62,28 @@
           }
     })
 
-    $('.owl-portfolio').owlCarousel({
-        items:4,
-        loop:true,
-        dots: true,
-        nav: true,
-        autoplay: true,
-        margin:30,
-          responsive:{
-              0:{
-                  items:1
-              },
-              700:{
-                  items:2
-              },
-              1000:{
-                  items:3
-              },
-              1600:{
-                  items:4
-              }
-          }
-    })
+    // $('.owl-portfolio').owlCarousel({
+    //     items:4,
+    //     loop:true,
+    //     dots: true,
+    //     nav: true,
+    //     autoplay: true,
+    //     margin:30,
+    //       responsive:{
+    //           0:{
+    //               items:1
+    //           },
+    //           700:{
+    //               items:2
+    //           },
+    //           1000:{
+    //               items:3
+    //           },
+    //           1600:{
+    //               items:4
+    //           }
+    //       }
+    // })
 
     
 
